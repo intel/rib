22ab847293ad72f8de9740cb77084e4bb19f755c
Geoff Gustafson <geoff@linux.intel.com>
2011-12-29 13:56:38 -0800

    [General] Fix indentation in palette.js

--- src/js/palette.js
+++ src/js/palette.js
@@ -26,56 +26,56 @@
  */
 
 function loadPalette(container, filename) {
-	var defaultContainer = '#palette-panel';
-	var myContainer = container;
+    var defaultContainer = '#palette-panel';
+    var myContainer = container;
 
-        if (!myContainer) {
-		myContainer = $(defaultContainer);
-        }
+    if (!myContainer) {
+	myContainer = $(defaultContainer);
+    }
 
-        if (!myContainer || !myContainer.get()) {
-		return false;
-        }
+    if (!myContainer || !myContainer.get()) {
+	return false;
+    }
 
-	return $.getJSON(filename, function(data){
-                console.log("Starting palette load...");
-		myContainer.append('<p id="palette_header" class="ui-helper-reset ui-widget ui-widget-header">Palette</p>');
-		myContainer.append('<ul id="palette_accordion"></ul>');
+    return $.getJSON(filename, function(data){
+        console.log("Starting palette load...");
+	myContainer.append('<p id="palette_header" class="ui-helper-reset ui-widget ui-widget-header">Palette</p>');
+	myContainer.append('<ul id="palette_accordion"></ul>');
 
-		var hdr = $('#palette_header');
-		var acc = $('#palette_accordion');
+	var hdr = $('#palette_header');
+	var acc = $('#palette_accordion');
 
-		// Make use of the flex box model in CSS3 to allow the tabs
-		// to grow/shrink with the window/container
-		myContainer.addClass('vbox');
-		hdr.addClass('flex0');
-		acc.addClass('flex1');
+	// Make use of the flex box model in CSS3 to allow the tabs
+	// to grow/shrink with the window/container
+	myContainer.addClass('vbox');
+	hdr.addClass('flex0');
+	acc.addClass('flex1');
 
-                // FIXME: Eventually, all widgets should come from the BWidget
-                //        global structure.  For now, we load them as their own
-                //        subcategory in the palette
-		$(acc).append('<li><p>Tizen Framework</p><ul id="Tizen-widgets"></ul></li>');
-                $.each(BWidget.getPaletteWidgetTypes(), function(n, id) {
-			// Add new <li> element to hold this widget
-			var ul = $('#Tizen-widgets');
-			var li = $('<li id="BWidget-'+id+'"></li>').appendTo($(ul));
-			$(li).button({
-				label: BWidget.getDisplayLabel(id),
-				icons: {primary: BWidget.getIcon(id)}
-				});
-			$(li).disableSelection();
-			$(li).addClass('nrc-palette-widget');
-			$(li).data("code", BWidget.getTemplate(id));
-			$(li).data("adm-node", {type: id});
-			$(ul).append($(li));
-                });
+        // FIXME: Eventually, all widgets should come from the BWidget
+        //        global structure.  For now, we load them as their own
+        //        subcategory in the palette
+	$(acc).append('<li><p>Tizen Framework</p><ul id="Tizen-widgets"></ul></li>');
+        $.each(BWidget.getPaletteWidgetTypes(), function(n, id) {
+	    // Add new <li> element to hold this widget
+	    var ul = $('#Tizen-widgets');
+	    var li = $('<li id="BWidget-'+id+'"></li>').appendTo($(ul));
+	    $(li).button({
+		label: BWidget.getDisplayLabel(id),
+		icons: {primary: BWidget.getIcon(id)}
+	    });
+	    $(li).disableSelection();
+	    $(li).addClass('nrc-palette-widget');
+	    $(li).data("code", BWidget.getTemplate(id));
+	    $(li).data("adm-node", {type: id});
+	    $(ul).append($(li));
+        });
 
-		$(acc).accordion({
-			fillSpace: true,
-			});
-
-		// Must explicitly react to window resize events to be
-		// able to grow/shrink if we're in a flex box layout
-		$(window).resize( function () { $(acc).accordion("resize"); });
+	$(acc).accordion({
+	    fillSpace: true,
 	});
+
+	// Must explicitly react to window resize events to be
+	// able to grow/shrink if we're in a flex box layout
+	$(window).resize( function () { $(acc).accordion("resize"); });
+    });
 }
