e944dfe1878e4419205a319a9b01813d37f02416
John Chen <zhang.z.chen@intel.com>
2012-02-14 23:22:33 +0800

    [Live View] Implement device selection
    Modified-by: Shane Bryan <shane.bryan@intel.com>
    - Re-worked devices.json to allow for future heirachical
      organization of devices by type

--- src/js/views/live.js
+++ src/js/views/live.js
@@ -17,11 +17,18 @@
 
         options: {
             model: null,
+            iframe: null,
+            contentDocument: null,
         },
 
         _create: function() {
             var o = this.options,
-                e = this.element;
+                controlPanel,
+                pageView,
+                devices,
+                deviceToolbar,
+                deviceSelect,
+                widget = this;
 
             o.designReset = this._designResetHander;
             o.selectionChanged = null;
@@ -36,9 +43,58 @@
                 this._bindADMEvents(o.model);
             }
 
-            $('<div/>').addClass(this.widgetName)
-                .text(this.widgetName)
+            controlPanel = $('<div/>')
+                .addClass('hbox')
+                .addClass('flex0');
+
+            pageView = $('<div/>')
+                .addClass('vbox')
+                .addClass('flex0')
+                .css({ "min-width": 300, })  // FIXME: do this in CSS
+                .pageView();
+            pageView.pageView('option', 'model', ADM);
+
+            devices = $('<div/>')
+                .addClass('vbox')
+                .addClass('flex1')
+                .append('<div><span>DEVICES</span></div>');
+
+            deviceToolbar = $('<div/>')
+                .css({
+                    backgroundColor: "#e4e5df",       // FIXME: do this in CSS
+                    borderLeft:"1px solid #8c8d8d",   // FIXME: do this in CSS
+                    borderBottom: "1px solid #b9b9b9" // FIXME: do this in CSS
+                 })
+                .appendTo(devices);
+
+            deviceSelect = $('<select></select>')
+                .appendTo(deviceToolbar)
+                .change(function () {
+                    $("option:selected", this).each(function () {
+                        widget._setDevice($(this).data('deviceInfo'));
+                    });
+            });
+
+            $.getJSON("src/assets/devices.json", function (data) {
+                $.each(data, function (key, val) {
+                    $('<option/>').append( key )
+                        .data('deviceInfo', val.Default)
+                        .appendTo(deviceSelect);
+                });
+                deviceSelect.trigger('change');
+            });
+
+            controlPanel.append(pageView)
+                .append(devices)
                 .appendTo(this.element);
+
+            this.options.iframe = $('<iframe/>')
+                .attr({id:'deviceScreen'})
+                .addClass('flex1')
+                .appendTo(this.element);
+
+            this.options.contentDocument =
+                $(this.options.iframe[0].contentDocument);
 
             this.options.primaryTools = this._createPrimaryTools();
             this.options.secondaryTools = this._createSecondaryTools();
@@ -70,33 +126,52 @@
         },
 
         refresh: function(event, widget) {
-            var stage = $('#liveView > .stage'),
-                deviceScreen = $('#deviceScreen'),
+            var iframe,
                 liveDoc;
 
             widget = widget || this;
-            if (deviceScreen.length === 0)
-            {
-                stage.empty();
-                deviceScreen = $('<iframe/>')
-                    .attr({id:'deviceScreen'})
-                    .addClass('phone')  // Default to phone skin
-                    .addClass('flex1')
-                    .appendTo(stage);
+            iframe = widget.options.iframe;
+            if (iframe.length) {
+                liveDoc = widget.options.contentDocument[0];
+                liveDoc.open();
+                liveDoc.writeln(generateHTML());
+                liveDoc.close();
+                iframe.load( function () {
+                    var page = widget.options.model.getActivePage() || null;
+                    if (page) {
+                        widget._setPreviewPage(page.getProperty('id'), widget);
+                    }
+                });
             }
-            liveDoc = deviceScreen.contents()[0];
-            liveDoc.open();
-            liveDoc.writeln(generateHTML());
-            liveDoc.close();
-            deviceScreen.load( function () {
-                var page = widget.options.model.getActivePage() || null;
-                if (page) {
-                    widget._setPreviewPage(page.getProperty('id'), widget);
-                }
-            });
         },
 
         // Private functions
+        _setDevice: function (info) {
+
+            // TODO: This may be better managed by reading and applying
+            //       per-device CSS files from the filesystem at run time.
+            //       By finding all *.css files under src/assets/devices, we
+            //       could simply change the element "class" or custom
+            //       attribute (data-device="<devicename>").  Then these
+            //       dimensional properties can be set by the UX team, and
+            //       never require changes to devices.json or this code...
+
+            this.options.iframe.css({
+                width: info.screen.width,
+                minWidth: info.screen.width,
+                maxWidth: info.screen.width,
+                height: info.screen.height,
+                minHeight: info.screen.height,
+                maxHeight: info.screen.height,
+                paddingTop: info.screen.offset.top,
+                paddingRight: info.screen.offset.right,
+                paddingBottom: info.screen.offset.bottom,
+                paddingLeft: info.screen.offset.left,
+                backgroundSize: info.skin.width+' '+info.skin.height,
+                backgroundImage: 'url('+info.skin.href+')',
+            });
+        },
+
         _createPrimaryTools: function() {
             return $(null);
         },
@@ -136,15 +211,13 @@
         },
 
         _setPreviewPage: function (pageId, widget) {
-            var deviceScreen, previewWindow;
+            var win;
+
             widget = widget || this;
+            win = widget.options.contentDocument[0].defaultView;
 
-            deviceScreen = $('#deviceScreen', widget.element);
-            previewWindow = deviceScreen[0].contentWindow;
-
-            if (previewWindow.$ && previewWindow.$.mobile) {
-                previewWindow.$.mobile.changePage("#" + pageId,
-                                                  {transition: "none"});
+            if (win && win.$ && win.$.mobile) {
+                win.$.mobile.changePage("#" + pageId, {transition: "none"});
             }
         },
     });
