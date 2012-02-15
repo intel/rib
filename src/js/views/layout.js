c4a33e779ffa4f5f3c31819c07da04c0a179f48a
Shane Bryan <shane.bryan@intel.com>
2012-02-14 22:01:26 -0800

    [Layout View] Initial port of Design View to plugin model
    Still no drag-n-drop from palette yet, but basic rendering
    and ADM event handling and rendering is working again.
    
    Also no longer re-rendering the entire ADM/DOM on every
    modelUpdated event anymore, which solves responsiveness and
    flickering issues.
    
    Signed-off-by: Shane Bryan <shane.bryan@intel.com>

--- src/js/views/layout.js
+++ src/js/views/layout.js
@@ -17,6 +17,8 @@
 
         options: {
             model: null,
+            iframe: null,
+            contentDocument: null,
         },
 
         _create: function() {
@@ -36,12 +38,37 @@
                 this._bindADMEvents(o.model);
             }
 
-            $('<div/>').addClass(this.widgetName)
-                .text(this.widgetName)
+            this.options.iframe = this.element.find('iframe');
+            if (!this.options.iframe.length) {
+                this.options.iframe = $('<iframe/>');
+            }
+
+            this.options.iframe.addClass(this.widgetName)
+                .addClass('flex1')
                 .appendTo(this.element);
 
             this.options.primaryTools = this._createPrimaryTools();
             this.options.secondaryTools = this._createSecondaryTools();
+
+            this.options.contentDocument =
+                $(this.options.iframe[0].contentDocument);
+
+            $(window).resize(this, function(event) {
+                var el = event.data.element,
+                    doc = event.data.options.contentDocument,
+                    iframe = event.data.options.iframe;
+                // Force resize of the stage when containing window resizes
+                el.height(el.parent().height());
+                el.find('div').height(el.parent().height());
+                // Force resize of the iframe when containing window resizes
+                iframe.height(doc.height());
+                iframe.css('min-height',
+                            el.height() - 2
+                            - parseFloat(iframe.css('margin-top'))
+                            - parseFloat(iframe.css('margin-bottom'))
+                            - parseFloat(iframe.css('padding-bottom'))
+                            - parseFloat(iframe.css('padding-bottom')));
+            });
 
             this.refresh(null, this);
 
@@ -54,8 +81,13 @@
                 // the "host"... using the functions mapped in widget options?
                 case 'model':
                     this._unbindADMEvents(value);
-                    this._bindADMEvents(value);
                     this.options.model = value;
+                    this._createDocument();
+                    this.options.iframe.load(this, function(event) {
+                        event.data._bindADMEvents(event.data.options.model);
+                        event.data.loaded = true;
+                        event.data.refresh(null, event.data);
+                    });
                     break;
                 default:
                     break;
@@ -70,7 +102,36 @@
         },
 
         refresh: function(event, widget) {
-            widget = widget || this;
+            var name;
+            widget = widget || event && event.data || this;
+            name = (event)?(event.name)?event.name:event.type:'';
+
+            if (!widget.loaded) return;
+
+            if (!event) {
+                widget._serializeADMDesignToDOM();
+            } else if (event.type === 'load') {
+                widget._serializeADMDesignToDOM();
+            } else {
+                switch (event.name) {
+                    case 'designReset':
+                        widget._serializeADMDesignToDOM();
+                        break;
+                    default:
+                        console.warn(widget.widgetName,
+                                     ':: Unexpected refresh request:',
+                                     event.name);
+                        return;
+                        break;
+                }
+            }
+
+            if (widget.options.contentDocument.length) {
+                widget.options.contentDocument[0].defaultView
+                    .postMessage('reload', '*');
+            } else {
+                console.error(widget.widgetName, ':: Missing contentDocument');
+            }
         },
 
         // Private functions
@@ -88,40 +149,177 @@
         },
 
         _bindADMEvents: function(a) {
-            var d = a && a.getDesignRoot();
+            this.designRoot = a && a.getDesignRoot();
             a.bind("designReset", this._designResetHandler, this);
             a.bind("selectionChanged", this._selectionChangedHandler, this);
             a.bind("activePageChanged", this._activePageChangedHandler, this);
-            d.bind("modelUpdated", this._modelUpdatedHandler, this);
+            this.designRoot.bind("modelUpdated",
+                                 this._modelUpdatedHandler, this);
         },
 
         _unbindADMEvents: function(a) {
-            var d = a && a.getDesignRoot();
+            this.designRoot = a && a.getDesignRoot();
             a.unbind("designReset", this._designResetHandler, this);
             a.unbind("selectionChanged", this._selectionChangedHandler, this);
             a.unbind("activePageChanged", this._activePageChangedHandler, this);
-            d.unbind("modelUpdated", this._modelUpdatedHandler, this);
+            this.designRoot.unbind("modelUpdated",
+                                   this._modelUpdatedHandler, this);
         },
 
         _designResetHandler: function(event, widget) {
             widget = widget || this;
+            this.designRoot = event.design ||
+                              widget.options.model.getDesignRoot();
             widget.refresh(event, widget);
         },
 
         _selectionChangedHandler: function(event, widget) {
+            var uid;
             widget = widget || this;
-            widget.refresh(event, widget);
+
+            // Always un-style currently selected nodes
+            widget.options.contentDocument.find('.ui-selected')
+                .removeClass('ui-selected');
+
+            // Only apply selection style changes on valid nodes
+            if (!event || (!event.uid && !event.node)) {
+                return;
+            }
+
+            // Normally, ADM node id is provided in event.uid
+            uid = event && event.uid;
+            // Fallback is to try event.node.getUid()
+            uid = uid || (event.node)?event.node.getUid():null;
+
+            if (uid) {
+                widget.options.contentDocument
+                    .find('.adm-node[data-uid=\''+uid+'\']')
+                    .not('[data-role=\'page\']')
+                    .addClass('ui-selected').first().each(function() {
+                        // Scroll selected node into view
+                        this.scrollIntoViewIfNeeded();
+                    });
+            }
         },
 
         _activePageChangedHandler: function(event, widget) {
+            var id, win,
+                newPage = event && event.page, curPage;
+
             widget = widget || this;
-            widget.refresh(event, widget);
+            curPage = widget.options.model.getActivePage();
+
+            // Only change if new page is valid
+            if (!newPage) {
+                return;
+            }
+            id = newPage.getProperty('id');
+
+            // Only change if new page not the current page
+            if (curPage && curPage.getUid() === id) {
+                return;
+            }
+
+            win = widget.options.contentDocument[0].defaultView;
+
+            if (win && win.$ && win.$.mobile) {
+                win.$.mobile.changePage('#'+id);
+            }
         },
 
         _modelUpdatedHandler: function(event, widget) {
+            var win;
+
             widget = widget || this;
-            widget.refresh(event, widget);
+
+            serializeADMSubtreeToDOM(event.node, null, widget._renderer);
+
+            win = widget.options.contentDocument[0].defaultView;
+
+            if (win && win.$ && win.$.mobile) {
+                win.$.mobile.activePage.page('destroy');
+                win.$.mobile.activePage.page();
+            } else {
+                console.error(widget.widgetName, ':: Missing contentDocument');
+            }
         },
 
+        _createDocument: function() {
+            var contents, doc;
+
+            if (!this.designRoot) return;
+
+            doc = this.options.contentDocument[0];
+            doc.open();
+            contents = this._serializeFramework(this.designRoot);
+            doc.writeln(contents);
+            doc.close();
+        },
+
+        _serializeFramework: function() {
+            var start, end, ret, headers;
+
+            headers = $.gb.getDesignHeaders();
+
+            start = '<!DOCTYPE html>\n <html><head><title>Page Title</title>\n';
+            end = "</head>\n<body>\n</body>\n</html>";
+
+            if (headers && headers.length > 0) {
+                ret = start + headers.join('\n') + end;
+            } else {
+                ret = start + end;
+            }
+
+            return ret;
+        },
+
+        _serializeADMDesignToDOM: function() {
+            this.options.contentDocument.find('body >  div[data-role="page"]')
+                .remove();
+            serializeADMSubtreeToDOM(this.designRoot, null, this._renderer);
+        },
+
+        _renderer: function (admNode, domNode) {
+            if (!domNode) {
+                return;
+            }
+
+            // Attach the ADM UID to the element as an attribute so the DOM-id
+            // can change w/out affecting our ability to index back into the
+            // ADM tree
+            // XXX: Tried using .data(), but default jQuery can't select on this
+            //      as it's not stored in the element, but rather in $.cache...
+            //      There exist plugins that add the ability to do this, but
+            //      they add more code to load and performance impacts on
+            //      selections
+            $(domNode).attr('data-uid',admNode.getUid());
+
+            // Add a special (temporary) class used by the JQM engine to
+            // easily identify the "new" element(s) added to the DOM
+            $(domNode).addClass('nrc-dropped-widget');
+
+            // NOTE: if we bring back non-link buttons, we may need this trick
+            // Most buttons can't be dragged properly, so we put them behind
+            // the associated span, which can be dragged properly
+            // if (!isLinkButton(admNode))
+            //     $(domNode).css("z-index", "-1");
+
+            $(domNode).addClass('adm-node');
+
+            // If this node is "selected", make sure it's class reflects this
+            if (admNode.isSelected()) {
+                $(domNode).addClass('ui-selected');
+            }
+
+            // If this node is a "container", make sure it's class reflects this
+            if (admNode.isContainer() || admNode.getType() === 'Header') {
+                $(domNode).addClass('nrc-sortable-container');
+                if (admNode.getChildrenCount() === 0) {
+                    $(domNode).addClass('nrc-empty');
+                } else {
+                    $(domNode).removeClass('nrc-empty');
+                }
+            }
+        },
     });
 })(jQuery);
