7c781e236bed19ddbf603470e1dbc2a867f8b386
Xu Zhang <xu.u.zhang@intel.com>
2012-02-24 15:40:41 +0800

    [Utils] add pageUtils to gb name space
    modify createNewPage parameter as a object, which can be used as
    to create page accroding to it value

--- src/js/pageTemplate.js
+++ src/js/pageTemplate.js
@@ -10,84 +10,103 @@
 "use strict";
 
 /**
- * Creates an new page according to page template.
+ * Global object to access ADM page utils.
  *
- * @param {ADMNode} design add page node as its child
- * @param {String} pageTemplate The page type currently is JQM or blank page.
- * @return {ADMNode} The page node, or null if the page type was invalid.
  */
-function createNewPage(design, pageTemplate) {
-    var newPage, child, that, i, types, type;
-    if (!design.instanceOf("Design")) {
-        console.log("Warning: only design node can add new page");
-        return null;
-    }
-
-    switch (pageTemplate) {
-        case "JQM":
-            newPage = addNode(design, 'Page');
-            if (!newPage) {
+$(function() {
+    var pageUtils = {
+        /**
+         * Creates an new page according to page configure.
+         *
+         * @param {Object} config The page configure to create new page.
+         * @return {ADMNode} The page node, or null if the page type was invalid.
+         */
+        createNewPage: function(config) {
+            var design = config.design || ADM.getDesignRoot(),
+                pageTemplate = config.pageTemplate || "Blank Page",
+                pageTitle = config.pageTemplate || "Default",
+                layout = config.layout || {Header: true, Footer: true},
+                newPage, child, that, i, types, type;
+            if (!design.instanceOf("Design")) {
+                console.error("Error: wrong design root passed in");
                 return null;
             }
-            types = ['Header', 'Content', 'Footer'];
-            for (i in types) {
-                that = addNode(newPage, types[i]);
-                if (!that) {
+
+            switch (pageTemplate) {
+                case "JQuery Mobile Page":
+                    // create New ADM page node
+                    newPage = addNode(design, 'Page');
+                    if (!newPage) {
+                        return null;
+                    }
+                    // set page title
+                    newPage.setProperty("id", pageTitle);
+                    //set page layout
+                    for (i in layout) {
+                        if (layout[p]) {
+                            types.push(p);
+                        }
+                    }
+                    types.push('Content');
+                    for (i in types) {
+                        that = addNode(newPage, types[i]);
+                        if (!that) {
+                            return null;
+                        }
+                    }
+                    break;
+                case "Recent Used Page":
+                    if (design.getChildrenCount() !== 1) {
+                        console.log("Warning: this isn't the last page");
+                        return null;
+                    }
+                    newPage = addNode(design,'Page');
+                    if (!newPage) {
+                        return null;
+                    }
+                    newPage.setProperty("id", pageTitle);
+                    //get the last page
+                    child = design.getChildren()[0];
+                    for (i in child.getChildren()) {
+                        type = child.getChildren()[i].getType();
+                        that = addNode(newPage,type);
+                    }
+                    break;
+                default:
+                    console.log("Warning: invalid page type while creating new page: " + pageTemplate);
+                    //return a blank page as default
+                    newPage = addNode(design,'Page');
+                    if (!newPage) {
+                        return null;
+                    }
+                    newPage.setProperty("id", pageTitle);
+                    that = addNode(newPage,'Content');
+                    break;
+            }
+            return newPage;
+
+            /**
+             * Adds given child object to parent
+             *
+             * @param {ADMNode} parent The parent object to be added.
+             * @param {String} child widget type
+             * @return {ADMNode} The node, or null if add failed.
+             */
+            function addNode(parent, type) {
+                design.suppressEvents(true);
+                var child = ADM.createNode(type);
+                if (!child) {
+                    design.suppressEvents(false);
                     return null;
                 }
+                parent.addChild(child, false);
+                design.suppressEvents(false);
+                return child;
             }
-            break;
-        case "blank":
-            newPage = addNode(design,'Page');
-            if (!newPage) {
-                return null;
-            }
-            that = addNode(newPage,'Content');
-            break;
-        case "last":
-            if (design.getChildrenCount() !== 1) {
-                console.log("Warning: this isn't the last page");
-                return null;
-            }
-            newPage = addNode(design,'Page');
-            if (!newPage) {
-                return null;
-            }
-            //get the last page
-            child = design.getChildren()[0];
-            for (i in child.getChildren()) {
-                 type = child.getChildren()[i].getType();
-                 that = addNode(newPage,type);
-            }
-            break;
-        default:
-            console.log("Warning: invalid page type while creating new page: " + pageTemplate);
-	    //return a blank page as default
-	    newPage = addNode(design,'Page');
-	    if (!newPage) {
-		return null;
-	    }
-	    that = addNode(newPage,'Content');
-	    break;
-    }
-    return newPage;
+        };
+    };
 
-    /**
-     * Adds given child object to parent
-     *
-     * @param {ADMNode} parent The parent object to be added.
-     * @param {String} child widget type
-     * @return {ADMNode} The node, or null if add failed.
-     */
-    function addNode(parent, type) {
-        design.suppressEvents(true);
-        var child = ADM.createNode(type);
-        if (!child) {
-            design.suppressEvents(false);
-            return null;
-        }
-        parent.addChild(child, false);
-        design.suppressEvents(false);
-        return child;
-    }
-}
+    /*******************  export pageUtils to $.gb **********************/
+    $.gb = $.gb || {};
+    $.gb.pageUtils = pageUtils;
+});
