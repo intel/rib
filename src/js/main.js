842ab7e13d0459406a49d0a33435ea1837a6af74
Geoff Gustafson <geoff@linux.intel.com>
2012-03-09 19:36:42 -0800

    [General] Shrink toolbar button text to better match UX
    Also change margin betweens settings and export button, and change text
    "Project Settings"

--- src/js/main.js
+++ src/js/main.js
@@ -361,7 +361,7 @@
             this.ui.extras = $('<div/>').appendTo(this.ui.navbar)
                 .addClass('tools-secondary flex1 hbox');
             $('<div/>').addClass('default-tools hbox').show()
-                .append('<a id="setProj">settings</a>')
+                .append('<a id="setProj">project settings</a>')
                 .append('<hr align=vertical>')
                 .append('<a id="exportProj">export</a>')
                 .appendTo(this.ui.extras);
