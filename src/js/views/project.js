7d85fda936c33c01a27b970d6873893ad7391023
Xiaoyang Yu (Max) <max.a.yu@intel.com>
2012-06-12 15:19:03 +0800

    Ask user to confirm deleting a project

--- src/js/views/project.js
+++ src/js/views/project.js
@@ -290,7 +290,10 @@
                         });
                     }
                 };
-                $.rib.pmUtils.deleteProject(pid, success);
+                var continueToDelete = confirm("Are you sure to delete it?");
+                if(continueToDelete) {
+                    $.rib.pmUtils.deleteProject(pid, success);
+                }
             };
             // draw project box
             box = $('<div/>').attr('id',pid)
