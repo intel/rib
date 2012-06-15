intel/rib

commit ba1b2650a2348e77697d45f20a30099eb0326015

[Palette View] Add "active" property for Navbar buttons and iconpos property in Navbar

src/js/serialize.js
@@ -47,7 +47,7 @@ var DEBUG = true,
             parentNode = null,
             template, props, id,
             selMap = {},  // maps selectors to attribute maps
-            attrName, attrValue, propDefault,
+            attrName, attrValue, propValue, propDefault,
             widget, regEx, wrapper, domNodes;
 
         // Check for valid node
@@ -124,17 +124,22 @@ var DEBUG = true,
             // Apply any special ADMNode properties to the template before we
             // create the DOM Element instance
             for (var p in props) {
-                attrValue = node.getProperty(p);
+                propValue = node.getProperty(p);
 
                 switch (p) {
                 case "type":
                     break;
                 default:
                     attrName = BWidget.getPropertyHTMLAttribute(type, p);
+                    if (typeof attrName  === "object") {
+                        var attrMap = attrName;
+                        attrName = attrMap.name;
+                        attrValue = attrMap.value[propValue];
+                    }
                     if (attrName) {
                         propDefault = BWidget.getPropertyDefault(type, p);
 
-                        if (attrValue !== propDefault ||
+                        if (propValue !== propDefault ||
                             BWidget.getPropertyForceAttribute(type, p)) {
                             selector = BWidget.getPropertyHTMLSelector(type, p);
                             if (!selector) {
@@ -154,22 +159,22 @@ var DEBUG = true,
                     break;
                 }
 
-                if (typeof attrValue === "string" ||
-                    typeof attrValue === "number") {
+                if (typeof propValue === "string" ||
+                    typeof propValue === "number") {
                     // reasonable value to substitute in template
                     regEx = new RegExp('%' + p.toUpperCase() + '%', 'g');
-                    if(typeof attrValue === "string") {
-                        attrValue = attrValue.replace(/&/g, "&amp;");
-                        attrValue = attrValue.replace(/"/g, "&quot;");
-                        attrValue = attrValue.replace(/'/g, "&#39;");
-                        attrValue = attrValue.replace(/</g, "&lt;");
-                        attrValue = attrValue.replace(/>/g, "&gt;");
+                    if(typeof propValue === "string") {
+                        propValue = propValue.replace(/&/g, "&amp;");
+                        propValue = propValue.replace(/"/g, "&quot;");
+                        propValue = propValue.replace(/'/g, "&#39;");
+                        propValue = propValue.replace(/</g, "&lt;");
+                        propValue = propValue.replace(/>/g, "&gt;");
                         // Append UID to assist with debugging
                         if ($.rib.debug('showuid') && p === 'text') {
-                            attrValue += ' '+uid;
+                            propValue += ' '+uid;
                         }
                     }
-                    template = template.replace(regEx, attrValue);
+                    template = template.replace(regEx, propValue);
                 }
             }
 

src/js/widgets.js
@@ -78,7 +78,9 @@
  *   2)    defaultValue: optional default value for the property, of the type
  *                       specified above
  *   3)   htmlAttribute: optional string with an HTML attribute name that this
- *                       property should be written to
+ *                       property should be written to or an object that
+ *                       contains the name and value mapping of the HTML
+ *                       attribute
  *   4)  forceAttribute: if true, always write out the HTML attribute even when
  *                       it is equal to the default value (default: false)
  *   5)    htmlSelector: optional selector to find the DOM nodes on which to
@@ -106,6 +108,9 @@
  *                       guaranteed that if you see data X again, you will be
  *                       going from 3 to 5 rows, and can make sense of the
  *                       data.)
+ *   9)        validIn:  Parent widget in which this property is valid
+ *
+ *  10)      invalidIn:  Parent widget in which this property is not valid
  *
  * @class
  */
@@ -438,6 +443,21 @@ var BWidgetRegistry = {
 
             }
         ],
+        properties: {
+            iconpos: {
+                type: "string",
+                options: [ "left", "top", "bottom", "right", "notext" ],
+                defaultValue: "top",
+                htmlAttribute: "data-iconpos",
+            }
+        },
+        init: function (node) {
+            // initial state is three buttons
+            var i;
+            for (i = 0; i < 3; i++) {
+                node.addChild(new ADMNode("Button"));
+            }
+        },
         events: {
             sortchange: function (e, ui) {
                 BWidget.getWidgetAttribute("Navbar", "rearrange")($(this),
@@ -551,7 +571,19 @@ var BWidgetRegistry = {
                 type: "string",
                 options: [ "left", "top", "bottom", "right", "notext" ],
                 defaultValue: "left",
-                htmlAttribute: "data-iconpos"
+                htmlAttribute: "data-iconpos",
+                invalidIn: "Navbar"
+            },
+            active: {
+                type: "boolean",
+                defaultValue: false,
+                htmlAttribute: {
+                    name: "class",
+                    value: {
+                        true: "ui-btn-active",
+                        false: ""
+                    }
+                }
             },
             theme: {
                 type: "string",
@@ -562,7 +594,8 @@ var BWidgetRegistry = {
             inline: {
                 type: "boolean",
                 defaultValue: "false",
-                htmlAttribute: "data-inline"
+                htmlAttribute: "data-inline",
+                invalidIn: "Navbar"
             },
             transition: {
                 type: "string",

src/js/adm.js
@@ -1838,6 +1838,13 @@ ADMNode.prototype.getProperties = function () {
     defaults = BWidget.getPropertyDefaults(this.getType());
     for (i in defaults) {
         if (defaults.hasOwnProperty(i)) {
+            var validIn = BWidget.getPropertySchema(this.getType(), i).validIn,
+                invalidIn =
+                    BWidget.getPropertySchema(this.getType(), i).invalidIn;
+            if (validIn && this.getParent().getType() !== validIn)
+                continue;
+            if (invalidIn && this.getParent().getType() === invalidIn)
+                continue;
             props[i] = this._properties[i];
             if (props[i] === undefined) {
                 props[i] = this.generateUniqueProperty(i);

commit 39bbd9c2f7dcff73f05c26ac4d645838022b75a5

[General] Fixes for pull request #44 - Navbar
- [Serialize] Fix attribute bug introduced in ba1b2650
- [Layout View] Use .ui-navbar in CSS selectors
- [Layout View] Minor CSS code formatting fix
- [Widgets] Simplify the navbar rearrangement code

Signed-off-by: Shane Bryan <shane.bryan@intel.com>

src/css/composer.css
@@ -111,9 +111,8 @@
 /*
 Add some padding some that outline can be seen when selected.
 */
-[data-role="navbar"] li.adm-node {
-    padding: 2px;
-    box-sizing: border-box;
+.ui-navbar li.adm-node.ui-selected a {
+    position: static;
 }
 
 .adm-node.ui-selected,
@@ -155,7 +154,7 @@ Add some padding some that outline can be seen when selected.
 .ui-footer[class*="ui-bar-"].ui-masked,
 .ui-select.ui-masked span,
 .ui-masked > .orig-adm-node:not(.ui-unmasked),
-[data-role=navbar] > ul > li.ui-masked > a,
+.ui-navbar > ul > li.ui-masked > a,
 .ui-masked {
     outline-width: 1px !important;
     outline-style: solid !important;
@@ -167,7 +166,7 @@ Add some padding some that outline can be seen when selected.
                         startColorstr=#99FCFCC7,
                         endColorstr=#99FCFCC7);
 }
-[data-role=navbar].ui-unmasked:not(.ui-sortable-helper),
+.ui-navbar.ui-unmasked:not(.ui-sortable-helper),
 ul.adm-node.ui-unmasked:not(.ui-sortable-helper),
 ol.adm-node.ui-unmasked:not(.ui-sortable-helper),
 fieldset.adm-node.ui-unmasked:not(.ui-sortable-helper),
@@ -220,7 +219,7 @@ form.ui-drag-header:before,
 div.ui-drag-header[class*="ui-grid"]:before,
 div.ui-drag-header[data-role="controlgroup"]:before,
 div.ui-drag-header[data-role="collapsible-set"]:before,
-div.ui-drag-header[data-role="navbar"]:before,
+div.ui-drag-header.ui-navbar:before,
 [data-role="listview"]:not([data-inset="false"]).ui-drag-header:before {
     margin-top: -10px;
     margin-left: -10px;
@@ -269,7 +268,6 @@ label.adm-editable span.adm-text-content::before,
 }
 
 /* Prevent li "bullet" from showing on navbar buttons during sort */
-body > li.ui-sortable-helper[class*="ui-block-"]
-{
+body > li.ui-sortable-helper[class*="ui-block-"] {
     list-style: none;
 }

src/js/serialize.js
@@ -52,7 +52,7 @@ var DEBUG = true,
             parentNode = null,
             template, props, id,
             selMap = {},  // maps selectors to attribute maps
-            attrName, attrValue, propValue, propDefault,
+            attrName, attrValue, propDefault,
             widget, regEx, wrapper, domNodes;
 
         // Check for valid node
@@ -129,7 +129,7 @@ var DEBUG = true,
             // Apply any special ADMNode properties to the template before we
             // create the DOM Element instance
             for (var p in props) {
-                propValue = node.getProperty(p);
+                attrValue = node.getProperty(p);
 
                 switch (p) {
                 case "type":
@@ -139,12 +139,12 @@ var DEBUG = true,
                     if (typeof attrName  === "object") {
                         var attrMap = attrName;
                         attrName = attrMap.name;
-                        attrValue = attrMap.value[propValue];
+                        attrValue = attrMap.value[attrValue];
                     }
                     if (attrName) {
                         propDefault = BWidget.getPropertyDefault(type, p);
 
-                        if (propValue !== propDefault ||
+                        if (attrValue !== propDefault ||
                             BWidget.getPropertyForceAttribute(type, p)) {
                             selector = BWidget.getPropertyHTMLSelector(type, p);
                             if (!selector) {
@@ -164,22 +164,22 @@ var DEBUG = true,
                     break;
                 }
 
-                if (typeof propValue === "string" ||
-                    typeof propValue === "number") {
+                if (typeof attrValue === "string" ||
+                    typeof attrValue === "number") {
                     // reasonable value to substitute in template
                     regEx = new RegExp('%' + p.toUpperCase() + '%', 'g');
-                    if(typeof propValue === "string") {
-                        propValue = propValue.replace(/&/g, "&amp;");
-                        propValue = propValue.replace(/"/g, "&quot;");
-                        propValue = propValue.replace(/'/g, "&#39;");
-                        propValue = propValue.replace(/</g, "&lt;");
-                        propValue = propValue.replace(/>/g, "&gt;");
+                    if(typeof attrValue === "string") {
+                        attrValue = attrValue.replace(/&/g, "&amp;");
+                        attrValue = attrValue.replace(/"/g, "&quot;");
+                        attrValue = attrValue.replace(/'/g, "&#39;");
+                        attrValue = attrValue.replace(/</g, "&lt;");
+                        attrValue = attrValue.replace(/>/g, "&gt;");
                         // Append UID to assist with debugging
                         if ($.rib.debug('showuid') && p === 'text') {
-                            propValue += ' '+uid;
+                            attrValue += ' '+uid;
                         }
                     }
-                    template = template.replace(regEx, propValue);
+                    template = template.replace(regEx, attrValue);
                 }
             }
 

src/js/widgets.js
@@ -467,29 +467,26 @@ var BWidgetRegistry = {
             },
         },
         rearrange: function (sortable, placeholder, excludePlaceholder) {
-            var classes = ['a', 'b', 'c', 'd', 'e', 'solo'], i = 0, blocks, zone,
-                replaceClass = function (elem, oldClassSuffix, newClass) {
-                    var reg = new RegExp ('\\b' + oldClassSuffix +  '\\S+', 'g');
-                    elem.removeClass(function (index, css) {
-                        return (css.match (reg) || []).join(' ');
-                    })
-                    .addClass(newClass);
-                };
+            var classes = ['a', 'b', 'c', 'd', 'e', 'solo'],
+                gridClasses = 'ui-grid-a ui-grid-b ui-grid-c ' +
+                              'ui-grid-d ui-grid-e ui-grid-solo',
+                blockClasses = 'ui-block-a ui-block-b ui-block-c ' +
+                               'ui-block-d ui-block-e ui-block-solo',
+                i = 0, j, pClass, blocks, zone;
 
-            if (sortable.is('[data-role="navbar"]')){
+            if (sortable.is('.ui-navbar')){
                 zone = sortable.find('ul');
-                if (excludePlaceholder)
-                    replaceClass(placeholder, 'ui-block-', '');
-                else
-                    placeholder.addClass('ui-block-a');
+                pClass = excludePlaceholder ? blockClasses : 'ui-block-a';
+                placeholder.toggleClass(pClass, !excludePlaceholder);
                 blocks = zone.children('[class*=ui-block-]:visible');
                 blocks.each( function () {
-                    if (blocks.length > 5)
-                        i = i % 2;
-                    replaceClass($(this), 'ui-block-', 'ui-block-' + classes[i++]);
+                    if (blocks.length > 5) i = i % 2;
+                    $(this).toggleClass(blockClasses, false)
+                           .toggleClass('ui-block-'+classes[i++], true);
                 });
-                replaceClass(zone, 'ui-grid-', 'ui-grid-' +
-                        classes[blocks.length > 5 ? 0 : (i - 2 < 0 ? 5 : i - 2)]);
+                j = (blocks.length>5) ? 0 : (i-2 < 0) ? 5 : i-2;
+                zone.toggleClass(gridClasses, false)
+                    .toggleClass('ui-grid-'+classes[j]);
             }
         }
     },