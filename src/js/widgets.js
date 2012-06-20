intel/rib

commit 7c1164e1bbadc73cd6849e35cc487fe89b5f28c3

[Widgets] Completed count bubble for ListButton.
Added a countBubble property for ListButton.

BTW: I think the ListButton should merge into ListItem, the link should be a property for ListItem, as same as count bubble.

src/js/widgets.js
@@ -1260,9 +1260,29 @@ var BWidgetRegistry = {
                 options: [ "default", "a", "b", "c", "d", "e" ],
                 defaultValue: "default",
                 htmlAttribute: "data-theme"
+            },
+            countbubble: {
+                type: "string",
+                displayName: "count bubble",
             }
         },
-        template: '<li><a>%TEXT%</a></li>'
+        template: function (node) {
+            var prop, countBubble, code = $('<li><a></a></li>');
+            var container = code.find('a');
+            container
+                .html(node.getProperty("text"))
+                .attr('href', node.getProperty("target"));
+            prop = node.getProperty("countbubble");
+            // Add the count bubble if countbubble property is not blank
+            if (prop.trim() != '') {
+                countBubble = $('<span>')
+                    .attr('class', 'ui-li-count')
+                    .html(prop);
+                container.append(countBubble);
+            };
+            return code;
+        }
+        
     },
 
     /**

commit e9fec0d8e0247751a068862835de2029c0ec5ba9

[Widgets] Fix bug introduced in 7c1164e1
New "countbubble" html attribute needs a defaultValue,
otherwise, the template function throws an error since
prop.trim() can be attempted when prop is undefined.

Signed-off-by: Shane Bryan <shane.bryan@intel.com>

src/js/widgets.js
@@ -1447,6 +1447,7 @@ var BWidgetRegistry = {
             countbubble: {
                 type: "string",
                 displayName: "count bubble",
+                defaultValue: ""
             }
         },
         template: function (node) {