f491378a1977356519653b833a8ce895ef9067c0
yongfeng wu <yongfengx.wu@intel.com>
2012-07-05 16:58:44 +0800

    [Widgets] Fixed Bug: The background image resizes upon insertion of many widgets, distorting itself

--- src/js/widgets.js
+++ src/js/widgets.js
@@ -222,9 +222,9 @@ var BWidgetRegistry = {
                     name: "style",
                     value: function (propValue) {
                         return "background-image:url('" + propValue + "');" +
-                            "background-attachment:scroll;" +
+                            "background-attachment:fixed;" +
                             "background-repeat:no-repeat;" +
-                            "background-size:cover;";
+                            "background-size:100% 100%;";
                     }
                 }
             }
