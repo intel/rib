db86bcb0849fd0b8ab50f759a0f09dd3529bbd94
John Chen <zhang.z.chen@intel.com>
2012-07-06 16:13:40 +0800

    [Live View] Page changes should trigger ADM page changes
    As Page Picker can change page in preiview, page changes in
    preview should also trigger page changes in Page Picker.

--- src/js/views/live.js
+++ src/js/views/live.js
@@ -304,6 +304,16 @@
                     getOwnerWindow(liveDoc.documentElement).$(liveDoc)
                         .bind('contextmenu', function(e) {
                             e.preventDefault();
+                        })
+                        .find('div:jqmData(role="page")')
+                        .bind('pageshow', function(e) {
+                            var pageId = this.id;
+                            $.each(ADM.getDesignRoot().getChildren(), function (i, child) {
+                                if (child.getProperty('id') == pageId) {
+                                    ADM.setActivePage(child);
+                                    return false;
+                                }
+                            });
                         });
                     $('body', liveDoc).css({
                         // prevent I bar cursor over UI text
