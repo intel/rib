e59195f2e8d693c2729a268971604141c0d06be0
John Chen <zhang.z.chen@intel.com>
2012-07-18 11:23:52 +0800

    [Layout View] Solve placeholder jittering problem

--- src/js/composer.js
+++ src/js/composer.js
@@ -565,6 +565,14 @@ $(function() {
                 },
                 change: function(event, ui){
                     trackOffsets('change:  ',ui,$(this).data('sortable'));
+
+                    // Workaround a jQuery UI's bug which sometimes rearranges
+                    // the placeholder to a container without changing
+                    // the currentContainer, which leads to placeholder
+                    // jittering
+                    $(this).data('sortable').currentContainer
+                        = ui.placeholder.closest('.nrc-sortable-container')
+                                        .data('sortable');
                     adjustMargins(ui.placeholder);
                 },
                 sort: function(event, ui){
