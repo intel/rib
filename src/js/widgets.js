7151c20bc898278abe41da734ffe30c8373083a8
John Chen <zhang.z.chen@intel.com>
2012-08-07 11:50:37 +0800

    [Widgets] Implement ordered list as a property of ListBase

--- src/js/widgets.js
+++ src/js/widgets.js
@@ -1598,7 +1598,6 @@ var BWidgetRegistry = {
                 htmlAttribute: "data-filter-placeholder"
             }
         },
-        template: '<ul data-role="listview">',
     },
 
     /**
@@ -1623,7 +1622,6 @@ var BWidgetRegistry = {
     IconList: {
         parent: "SimpleList",
         paletteImageName: "jqm_icon_list.svg",
-        template: '<ul data-role="listview">',
     },
 
     /**
@@ -1632,7 +1630,6 @@ var BWidgetRegistry = {
     ThumbnailList: {
         parent: "SimpleList",
         paletteImageName: "jqm_thumbnail_list.svg",
-        template: '<ul data-role="listview">',
     },
     /**
      * Represents a base List widget
@@ -1644,6 +1641,10 @@ var BWidgetRegistry = {
             inset: BCommonProperties.inset,
             filter: BCommonProperties.filter,
             theme: BCommonProperties.theme,
+            ordered: {
+                type: "boolean",
+                defaultValue: false,
+            },
             divider_theme: $.extend({}, BCommonProperties.theme, {
                 displayName: "divider theme",
                 htmlAttribute: "data-divider-theme"
@@ -1689,6 +1690,12 @@ var BWidgetRegistry = {
             // Reconstruct the domNode.
             return filterForm.wrap(newNode).parent().append(domNode);
         },
+        template: function (node) {
+            if (node.getProperty("ordered"))
+             return $('<ol data-role="listview">');
+            else
+             return $('<ul data-role="listview">');
+        },
         init: function (node) {
             // initial state is three button ListItem
             var i;
@@ -1699,15 +1706,6 @@ var BWidgetRegistry = {
                 node.addChild(ADM.createNode(itemName));
             }
         }
-    },
-
-    /**
-     * Represents an ordered list element.
-     */
-    OrderedList: {
-        parent: "ListBase",
-        paletteImageName: "jqm_ordered_list.svg",
-        template: '<ol data-role="listview">',
     },
 
     /**
@@ -1742,7 +1740,6 @@ var BWidgetRegistry = {
                 htmlAttribute: "data-split-icon"
             }
         },
-        template: '<ul data-role="listview">',
     },
 
     /**
