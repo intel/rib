95f2ebe986dfa692b2e9dc955f6457cc54cee3d5
John Chen <zhang.z.chen@intel.com>
2012-08-27 12:58:34 +0800

    [Widgets] Change iconsrc property display name to Icon Source

--- src/js/widgets.js
+++ src/js/widgets.js
@@ -1772,7 +1772,7 @@ var BWidgetRegistry = {
             }
         },
         template: function(node) {
-            var prop, iconsrc, countBubble, code = $('<li></li>');
+            var prop, countBubble, code = $('<li></li>');
             prop = node.getProperty("countbubble");
             // Add the count bubble if countbubble property is not blank
             if (prop.trim() != '') {
@@ -1892,6 +1892,7 @@ var BWidgetRegistry = {
                 defaultValue: "Icon List Item"
             },
             iconsrc: {
+                displayName: "icon source",
                 type: "url-uploadable",
                 defaultValue: "src/css/images/widgets/tizen_image.svg",
                 htmlSelector: "img",
