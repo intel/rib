intel/rib

commit e5be2c04815ca52e1d22616375e29fb47298f000

[Property View] Fix bug: Two types of form widget are used for Grid's integer properties

src/js/views/property.js
@@ -113,7 +113,8 @@
                 design = ADM.getDesignRoot(),
                 title = this.element.parent().find('.property_title'),
                 content = this.element.find('.property_content'),
-                continueToDelete, buttonsContainer, container, prerequisite;
+                continueToDelete, buttonsContainer, container, prerequisite,
+                range, min, max, input;
 
             // Clear the properties pane when nothing is selected
             if (node === null || node === undefined) {
@@ -179,6 +180,36 @@
                         if ((node.getProperty (p) === true) ||
                             (node.getProperty (p) === "true")) {
                             value.find("#" + valueId).attr("checked", "checked");
+                        }
+                        break;
+                    case "integer":
+                        range = BWidget.getPropertyRange(type, p);
+                        if (range) {
+                            min = range.min;
+                            max = range.max;
+                            input = $('<input type="number"/>')
+                                .addClass('title labelInput')
+                                .attr({
+                                    id: valueId
+                                })
+                                .change(function(event) {
+                                    if( typeof this.max !== "undefined" &&
+                                        parseInt(this.value) > parseInt(this.max) ||
+                                        typeof this.min !== "undefined" &&
+                                        parseInt(this.value) < parseInt(this.min)) {
+                                            $(this).effect("highlight",
+                                                {color: "red"}, 1000);
+                                            event.stopImmediatePropagation();
+                                            this.value = valueVal;
+                                    }
+                                })
+                                .appendTo(value);
+                            if (typeof min !== "undefined")
+                                input.attr("min", min);
+                            if (typeof max !== "undefined")
+                                input.attr("max", max);
+                            //set default value
+                            value.find('#' + valueId).val(valueVal);
                         }
                         break;
                     case "url-uploadable":

src/js/widgets.js
@@ -192,7 +192,11 @@ var BCommonProperties = {
  *  11)      invalidIn:  Parent widget in which this property is not valid
  *  12)        visible:  optional boolean for the property user-exposed in
  *                       property view (default true)
- *
+ *  13)        range:    optional object to define the value range of the
+ *                       property. For example { min: 2, max: 5 } means
+ *                       The minimum value is 2 and maximum value is 5. If
+ *                       min/max is not specified, it means no limit for
+ *                       minimum/maximum value respectively.
  * @class
  */
 var BWidgetRegistry = {
@@ -1754,18 +1758,12 @@ var BWidgetRegistry = {
             rows: {
                 type: "integer",
                 defaultValue: 1,
+                range: { min: 1, max:15 },
                 setPropertyHook: function (node, value, transactionData) {
                     var rows, columns, i, block, map, children, blocks, count,
                         blockIndex, root;
                     rows = node.getProperty("rows");
                     columns = node.getProperty("columns");
-
-                    // FIXME: really this should be enforced in the property
-                    //        pane, or elsewhere; this won't really work
-                    if (value < 1) {
-                        value = 1;
-                    }
-
                     root = node.getDesign();
                     root.suppressEvents(true);
 
@@ -1819,8 +1817,8 @@ var BWidgetRegistry = {
             },
             columns: {
                 type: "integer",
-                options: [ 2, 3, 4, 5 ],
                 defaultValue: 2,
+                range: { min: 2, max: 5},
                 setPropertyHook: function (node, value, transactionData) {
                     var rows, columns, i, block, map, children, blocks, count,
                         index, blockIndex, root;
@@ -2556,6 +2554,21 @@ var BWidget = {
         var schema = BWidget.getPropertySchema(widgetType, property);
         if (schema) {
             return schema.htmlValueMap;
+        }
+        return schema;
+    },
+
+    /**
+     * Gets the range for a given property.
+     *
+     * @param {String} widgetType The type of the widget.
+     * @param {String} property The name of the requested property.
+     * @return {Object} The range of the value of the given property
+     */
+    getPropertyRange: function (widgetType, property) {
+        var schema = BWidget.getPropertySchema(widgetType, property);
+        if (schema) {
+            return schema.range;
         }
         return schema;
     },

commit c230be164baa7329c17da0618bb69e2822bbb7e5

[Property View] Fix range reset bug introduced in e5be2c04
Signed-off-by: Shane Bryan <shane.bryan@intel.com>

src/js/views/property.js
@@ -443,7 +443,7 @@
                                 .attr({
                                     id: valueId
                                 })
-                                .change(function(event) {
+                                .change(node, function(event) {
                                     if( typeof this.max !== "undefined" &&
                                         parseInt(this.value) > parseInt(this.max) ||
                                         typeof this.min !== "undefined" &&
@@ -451,7 +451,7 @@
                                             $(this).effect("highlight",
                                                 {color: "red"}, 1000);
                                             event.stopImmediatePropagation();
-                                            this.value = valueVal;
+                                            this.value = event.data.getProperty(this.id.slice(0, this.id.indexOf('-')));
                                     }
                                 })
                                 .appendTo(value);